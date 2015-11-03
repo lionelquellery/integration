52620c12a0a88e350e287dd2825c989b50a81a7e
lionelquellery <lionel.quellery@hetic.net>
2015-11-03 17:16:50 +0100

    update css/php

--- header-single.php
+++ header-single.php
@@ -14,5 +14,5 @@
 <body> 
 <!-- Header general -->
  <header>
-    <img src="<?php echo get_template_directory_uri(); ?>/img/logo-wordpress.png" alt="Logo">
+    <a href="/"><img src="<?php echo get_template_directory_uri(); ?>/img/logo-wordpress.png" alt="Logo"></a>
     
--- header.php
+++ header.php
@@ -9,5 +9,5 @@
     <?php wp_head(); ?>
     
 </head>
-    <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo-wordpress.png" alt="Logo">
+    <a href="/"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo-wordpress.png" alt="Logo"></a>
 
