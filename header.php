d7aa6b8ba0723527a95f6d9220854bd3de191750
lionelquellery <lionel.quellery@hetic.net>
2015-11-03 19:52:25 +0100

    update css/php

--- header.php
+++ header.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <title><?php bloginfo('name'); ?></title>
 
-    <?php if(preg_match('`montagne`', $_SERVER['REQUEST_URI'])) {
-    	echo 'tst';
+    <?php if(preg_match('`ile-deserte`', $_SERVER['REQUEST_URI'])) {
+    	echo '<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/ile-deserte.css">';
     }
     ?>
-  	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
 
 
     <?php wp_head(); ?>
