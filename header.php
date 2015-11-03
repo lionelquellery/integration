109bccd7bdf3afaf8452ef44b9090cd83ccaadf9
lionelquellery <lionel.quellery@hetic.net>
2015-11-03 17:53:02 +0100

    update css/php

--- header.php
+++ header.php
@@ -9,5 +9,5 @@
     <?php wp_head(); ?>
     
 </head>
-    <a href="/"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo-wordpress.png" alt="Logo"></a>
+    <a href="/wordpress"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo-wordpress.png" alt="Logo"></a>
 
