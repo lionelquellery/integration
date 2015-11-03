43fa3511a37f5c27eaf497464b34f0cbd9e16bfd
lionelquellery <lionel.quellery@hetic.net>
2015-11-03 19:35:46 +0100

    update css/php

--- header.php
+++ header.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title><?php bloginfo('name'); ?></title>
+
+    <?php if(preg_match('`montagne`', $_SERVER['REQUEST_URI'])) {
+    	echo 'tst';
+    }
+    ?>
   	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
 
 
