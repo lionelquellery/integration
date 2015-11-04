d1dd766e889e65448b875a326d3608e6bcb7e48d
lionelquellery <lionel.quellery@hetic.net>
2015-11-04 22:47:29 +0100

    update view /html

--- header-createkit.php
+++ header-createkit.php
@@ -14,4 +14,4 @@
 <body> 
 <!-- Header general -->
  <header>
-    <a href="/wordpress"><img src="<?php echo get_template_directory_uri(); ?>/img/logo-wordpress.png" alt="Logo"></a>
+    <a href="/wordpress"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo-wordpress.png" alt="Logo"></a>
