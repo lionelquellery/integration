323cdcc873e6b15fe04df7f36edb2db698e0a6a9
lionelquellery <lionel.quellery@hetic.net>
2015-11-04 19:03:02 +0100

    update view

--- kit.php
+++ kit.php
@@ -52,6 +52,7 @@ if (isset($_POST['submit'])) {
    
    function DoSubmit(){
   document.myform.submit.value = 'Merci ';
+   window.location.replace("http://www.google.com");
   return true;
 }
 </script>
