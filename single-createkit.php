c91cd92de4445097bec2b67aa348c41811ec25cc
lionelquellery <lionel.quellery@hetic.net>
2015-11-04 22:40:16 +0100

    update view /html

--- single-createkit.php
+++ single-createkit.php
@@ -1,8 +1,8 @@
 <?php get_header('createkit'); ?>	
 
 <article>
-  <h1><?php the_field('situation-title'); ?></h1>
-<p><?php the_field('content_id'); ?></p>
+  <h1 class="title"><?php the_field('situation-title'); ?></h1>
+<p class="content"><?php the_field('content_id'); ?></p>
 </article>
 
 <?php get_footer(); ?>
