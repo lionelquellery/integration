81c7b8d02614310de42166f1e241565338e32bf4
lionelquellery <lionel.quellery@hetic.net>
2015-11-04 13:56:58 +0100

    update animation / js

--- js/index.js
+++ js/index.js
@@ -1,42 +1,12 @@
 
-   // var icon = $('.icon');
-   // var context = $('situation');
-   // var li = $('li');
-
-
-
-// $("enviro").each(function(index, element){
-//     var imagen = $(".icon");
-//     var overlay = $(this).find(".overlay");
-    // var texto1 = $(this).find(".situation");
-    // var texto2 = $(this).find(".textoBox2");
-
-
-//    var tl = new TimelineLite({paused:true});
-//     tl.to(imagen, 2, { css:{display:block},scale: 1.2, ease: Power4.easeOut})
-//       .to(overlay, .4, { bottom: 0, ease:Power3.easeInOut},".3")
-//       .to(texto1, .4, { bottom: 65, autoAlpha: 1, ease:Power2.easeOut},".5")
-//       .to(texto2, .4, { bottom: 30, autoAlpha: 1, ease:Power2.easeOut},".7");
-//   element.animation = tl;
-// });
-
-// $("enviro").hover(over, out);
-// function over(){ this.animation.play() };
-// function out(){ this.animation.reverse() };
-
-
-// li.mouseleave(function()
-// {
-//   var currentTime = tn1.time();
-//   tn1.reverse(currentTime);
-// });
+ document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+  });
 
 window.onload = (function animation() {
 
     // animation des blocks li
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
-  });
+
 
     var home = function home() {
 
