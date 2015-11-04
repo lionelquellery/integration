c2f37e3c67f7b748841992fe9a2d49f5ba4bd32c
lionelquellery <lionel.quellery@hetic.net>
2015-11-04 21:50:54 +0100

    update view /html

--- header-form.php
+++ header-form.php
@@ -5,5 +5,8 @@
 	<title><?php bloginfo('name'); ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/formpost.css">
 </head>
+<header>
 
+<button class="CreateKit"><a href="/wordpress">Retour</a></button>
+</header>
 
