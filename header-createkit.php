21c18e941655150fa2958c1437ed093278eccc79
lionelquellery <lionel.quellery@hetic.net>
2015-11-04 22:52:38 +0100

    update view /html

--- header-createkit.php
+++ header-createkit.php
@@ -14,4 +14,7 @@
 <body> 
 <!-- Header general -->
  <header>
+ <button class="CreateKit"><a href="/wordpress">Retour</a></button>
+
     <a href="/wordpress"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo-wordpress.png" alt="Logo"></a>
+    </header>
