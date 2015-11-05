b1f26a6c2b94544fb45d7d1824a4cd25ef9abe5d
lionelquellery <lionel.quellery@hetic.net>
2015-11-05 11:17:06 +0100

    update view /html

--- single-createkit.php
+++ single-createkit.php
@@ -5,6 +5,10 @@
 <p class="content"><?php the_field('content_id'); ?></p>
 <hr>
 <p>by <?php the_title(); ?></p>
+<ul class="share">
+	<li class="social"><img src="<?php echo get_template_directory_uri(); ?>/img/fb.svg" alt=""></li>
+	<li class="social"><img src="<?php echo get_template_directory_uri(); ?>/img/tw.svg" alt=""></li>
+</ul>
 </article>
 
 <?php get_footer(); ?>
