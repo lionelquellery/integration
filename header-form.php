24c552f4f3fa9bf342c44f6dcc3e5d3df3a2e505
lionelquellery <lionel.quellery@hetic.net>
2015-11-05 10:03:04 +0100

    update view /html

--- header-form.php
+++ header-form.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<title><?php bloginfo('name'); ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/formpost.css">
-	    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon.ico." />
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico." />
 
 </head>
 
--- header-single.php
+++ header-single.php
@@ -7,7 +7,7 @@
  
 
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
-	    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon.ico." />
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico." />
 
 
     <?php wp_head(); ?>
